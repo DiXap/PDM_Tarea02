f0ef7961462833946269f47ce88a4b8b94b7fccc
DiXa <dpadlara@ciencias.unam.mx>
2024-03-03 19:19:53 -0600

    Initial commit

---
+++ app/src/main/java/com/example/tarea02/asciiArt.java
@@ -0,0 +1,17 @@
+package com.example.tarea02;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Clase principal para la Tarea 02
+ * @author Diego J. Padilla
+ */
+public class asciiArt extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_layout);
+    }
+}
